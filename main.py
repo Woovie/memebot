cb3ddfd7f37501a8be5ce5841de5b01c4f2489f5
Jordan Banasik <woovie@woovie.net>
2020-06-23 23:49:38 -0700

    Added mongodb, uuids, usage tracking

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-import configparser, discord, glob, random
+import configparser, discord, glob, random, pymongo, uuid
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+mong_client = pymongo.MongoClient()
+
+db = mong_client['memebot']
+
+collection = db['memecount']
 
 class DiscordClient(discord.Client):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
     
     async def on_message(self, message):
-        print("on message")
         if message.content == config['discord']['cmd']:
-            print("content matched")
             meme = Meme()
             meme.select()
-            loaded_meme = discord.File(meme.meme)
-            await message.channel.send(file=loaded_meme)
+            if meme.meme:
+                loaded_meme = discord.File(meme.meme)
+                meme_uuid = str(uuid.uuid5(uuid.NAMESPACE_DNS, meme.meme))
+                query = {"uuid": meme_uuid}
+                result = collection.find(query)
+                for item in result:
+                    print(item)
+                if len(result) == 0:
+                    insertable = {"uuid": meme_uuid, "count": 1}
+                    collection.insert(insertable)
+                else:
+                    insertable = result[0]
+                    insertable["count"] = insertable["count"] + 1
+                    collection.update_one(query, insertable)
+                await message.channel.send(file=loaded_meme)
+            else:
+                await message.channel.send("Failed to load any memes! Check logs.")
 
 class Meme():
     def __init__(self):
         self.meme_folder = config['memes']['folder']
-        print(self.meme_folder)
         self.meme_list = glob.glob(f"{self.meme_folder}/*")
         self.meme = None
     
     def select(self):
         if len(self.meme_list) > 0:
-            print(f"some amount of memes found: {len(self.meme_list)}")
-            print(self.meme_list)
             self.meme = self.meme_list[random.randint(1, len(self.meme_list))-1]
-        else:
-            print('aaaaa')
         
 
 client = DiscordClient()
